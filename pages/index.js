14618480b5b7c88659e88661cc2183d544f9b2dd
root <root@localhost.localdomain>
2019-07-17 21:08:59 -0400

    release

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import MuiLink from '@material-ui/core/Link';
+import ProTip from '../src/ProTip';
+import Link from '../src/Link';
+import fetch from "isomorphic-unfetch"
+
+function MadeWithLove() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Built with love by the '}
+      <MuiLink color="inherit" href="https://material-ui.com/">
+        Material-UI
+      </MuiLink>
+      {' team.'}
+    </Typography>
+  );
+}
+
+const Index =props=> {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+      <div>
+      <h1>Batman TV Shows</h1>
+      <ul>
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js v4-beta example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <ProTip />
+        <MadeWithLove />
+      </Box>
+    </Container>
+  );
+}
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+export default Index
